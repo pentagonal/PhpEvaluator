5b80b4338f343e3e365102dc3632b4a90f88331f
pentagonal <pentagonaldevelopment@gmail.com>
2017-11-10 20:20:09 +0700

    revert

--- Src/Evaluator.php
+++ Src/Evaluator.php
@@ -23,122 +23,153 @@
  * SOFTWARE.
  */
 
-namespace Pentagonal\Modular;
+declare(strict_types=1);
 
-use Pentagonal\Modular\Override\SplFileInfo;
-use Pentagonal\PhpEvaluator\BadSyntaxExceptions;
-use Pentagonal\PhpEvaluator\Evaluator;
+namespace Pentagonal\PhpEvaluator;
 
 /**
- * Class PhpFileEvaluator
- * @package Pentagonal\Modular
- * @final
+ * Class Evaluator
+ * @package Pentagonal\PhpFileEvaluator
  */
-final class PhpFileEvaluator
+final class Evaluator
 {
-    /**
-     * @var SplFileInfo
-     */
-    protected $spl;
-
-    const PENDING    = 0;
-    const OK         = true;
-    const E_TAG      = Evaluator::E_TAG;
-    const E_BUFFER   = Evaluator::E_BUFFER;
-    const E_PARSE    = Evaluator::E_PARSE;
-    const E_ERROR    = Evaluator::E_ERROR;
+    const E_TAG      = E_STRICT;
+    const E_BUFFER   = E_WARNING;
+    const E_PARSE    = E_PARSE;
+    const E_ERROR    = E_COMPILE_ERROR;
 
     /**
-     * @var int|bool
-     */
-    protected $status = self::PENDING;
-
-    /**
-     * @var \Exception
-     */
-    protected $errorExceptions;
-
-    /**
-     * PhpFileEvaluator constructor.
+     * Suppress fallback
      *
-     * @param SplFileInfo $spl
+     * @param bool $suppress
      */
-    public function __construct(SplFileInfo $spl)
+    private function suppressErrorReport(bool $suppress = true)
     {
-        $this->spl = $spl;
+        static $error_reporting;
+        static $log_errors;
+        if (!isset($error_reporting)) {
+            $error_reporting = error_reporting();
+            $log_errors      = ini_get('log_errors');
+        }
+
+        $reporting = $suppress ? 0 : $error_reporting;
+        $logging   = $suppress ? 'off' : $log_errors;
+        error_reporting($reporting);
+        ini_set('log_errors', $logging);
     }
 
     /**
-     * @return bool|int
+     * Testing string with @uses token_get_all()
+     *
+     * @param string $content content to check
+     *
+     * @return array|bool
      */
-    public function getStatus()
+    private function tokenTestReport(string $content)
     {
-        return $this->status;
+        // suppress warning
+        $this->suppressErrorReport(true);
+        token_get_all($content);
+        // fall back to original
+        $this->suppressErrorReport(false);
+        // get last error
+        $lastError = error_get_last();
+        // if not empty & last error is current execution
+        $trace = is_array($lastError)
+                 && $lastError['file'] === __FILE__
+                 && $lastError['line'] < __LINE__
+                 && $lastError['line'] > (__LINE__ - 10) # on function
+            ? debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
+            : null;
+        $trace = !empty($trace[0]) ? $trace[0] : null;
+        if (is_array($trace)
+            && !empty($trace['class'])
+            && $trace['class'] === __CLASS__
+            && $trace['function'] === __FUNCTION__
+            && $trace['file'] === __FILE__
+            && !empty($lastError = error_get_last())
+        ) {
+            // clear last errors -> the token_get_all() error
+            error_clear_last();
+            return $lastError;
+        }
+
+        return false;
     }
 
     /**
-     * @return \Exception|null returning null if has no error
-     */
-    public function getException()
-    {
-        return $this->validate()->errorExceptions;
-    }
-
-    /**
-     * @return PhpFileEvaluator
+     * Check & Validate php code by string, this only works on some certains conditions
+     * because this is simple php code evaluator.
+     *
+     * @param string $content   string that php full of php code
+     * @param string|null $file file to append as error file info
+     *
+     * @return bool
+     * @throws BadSyntaxExceptions
      * @throws \Throwable
      */
-    public function validate() : PhpFileEvaluator
+    public static function check(string $content, string $file = null)
     {
-        if ($this->status !== self::PENDING) {
-            return $this;
+        $suffix = '};';
+        if (stripos($content, '<?php') !== 0) {
+            throw new BadSyntaxExceptions(
+                'Invalid open tag that not start with <?php or maybe contains white space',
+                self::E_TAG,
+                $file?:__FILE__,
+                1
+            );
+        }
+
+        $object = new self();
+
+        /* ------------------------------------------------
+         * TEST TOKEN
+         * ---------------------------------------------- */
+        if (is_array($lastError = $object->tokenTestReport($content))
+            && !empty($lastError['message'])
+        ) {
+            $line = 0;
+            // on last error get line
+            if (preg_match('/(.*)\s*starting\s*line\s*([0-9]+)\s*$/i', $lastError['message'], $match)
+                && !empty($match[2])
+            ) {
+                $lastError['message'] = $match[1];
+                $lastError['line'] = $match[2];
+            }
+
+            throw new BadSyntaxExceptions(
+                $lastError['message'],
+                self::E_ERROR,
+                $file?:__FILE__,
+                $line
+            );
+        }
+
+        if (($endTag = substr(rtrim($content), -2)) === '?>') {
+            if (substr($content, -2) !== $endTag) {
+                throw new BadSyntaxExceptions(
+                    'File content buffer on after closing php tag',
+                    self::E_BUFFER,
+                    $file ?:__FILE__,
+                    strrpos($content, '?>') + 1
+                );
+            }
+            $suffix = "<?php\n{$suffix}";
         }
 
         try {
-            Evaluator::check(
-                $this->spl->getContents(),
-                $this->spl->getRealPath()
-            );
-            $this->status = self::OK;
-        } catch (BadSyntaxExceptions $e) {
-            $this->errorExceptions = $e;
-            $this->status = $e->getCode();
+            // evaluate
+            eval("return true; if (0) { ?>{$content}\n{$suffix}");
         } catch (\Throwable $e) {
-            $this->status = self::E_ERROR;
-            $this->errorExceptions = $e;
-            return $this;
+            throw new BadSyntaxExceptions(
+                $e->getMessage(),
+                self::E_PARSE,
+                $file ?:__FILE__,
+                $e->getLine(),
+                $e
+            );
         }
 
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isValid() : bool
-    {
-        return $this->validate()->getStatus() === self::OK;
-    }
-
-    /**
-     * Create checker from file path
-     *
-     * @param string $filePath
-     *
-     * @return PhpFileEvaluator
-     */
-    public static function fromFile(string $filePath) : PhpFileEvaluator
-    {
-        return self::create(new SplFileInfo($filePath));
-    }
-
-    /**
-     * @param SplFileInfo $info
-     *
-     * @return PhpFileEvaluator
-     */
-    public static function create(SplFileInfo $info) : PhpFileEvaluator
-    {
-        return new self($info);
+        return true;
     }
 }
